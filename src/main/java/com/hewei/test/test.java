b3742747aebc2c60370bf161eb679a47567748d6
silenceHeWei <33090486+silenceHeWei@users.noreply.github.com>
2018-01-29 14:40:10 +0800

    测试

--- src/main/java/com/hewei/test/test.java
+++ src/main/java/com/hewei/test/test.java
@@ -4,14 +4,14 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class test extends HttpServlet{
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        System.out.println(username);
-        res.getWriter().print("6676");
-        ;
+        FileReader reader;
+
     }
 }
---
+++ src/main/java/com/hewei/testMainn.java
@@ -0,0 +1,13 @@
+package com.hewei;
+
+import java.io.File;
+import java.io.Serializable;
+
+public class testMainn implements Serializable {
+    public static void main(String[] args) {
+        int a = 666;
+        File child = new File("child");
+
+
+    }
+}
